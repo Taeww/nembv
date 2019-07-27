6e232f8f5279227e052d7479049a42becf3dc9bc
Kimtaewoo <ktw1572@gmail.com>
2019-07-27 17:59:59 +0900

    init

---
+++ routes/api/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const test = require('./test');
+
+router.all('*', (req, res, next) => {
+    console.log(req.path + ' welcome to api');
+
+    next();
+});
+
+router.use('/test', test);
+
+router.all('*', (req, res) => {
+    res.status(404).send({ success: false, msg: `unkown url ${req.path}` });
+});
+module.exports = router;
